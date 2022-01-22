5f2212eb82b9ae41a5364f11d2db10da1d6e07a7
Thilakraj Shetty H <thilakrajshetty08@gmail.com>
2022-01-23 01:41:24 +0530

    husky added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React from 'react'
+import Counter from './components/Counter'
 import IMAGES from './images/react.png'
-import './styles.css';
+import './styles.css'
 
 export const App = () => {
-    return (
-        <React.Fragment>
-            <h1>Hello React application - {process.env.REACT_APP_NAME}</h1>
-            <img src={IMAGES} alt=""/>
-            <Counter initialCounter={0}/>
-        </React.Fragment>
-    )
-};
+  return (
+    <React.Fragment>
+      <h1>Hello React application - {process.env.REACT_APP_NAME}</h1>
+      <img src={IMAGES} alt="" />
+      <Counter initialCounter={0} />
+    </React.Fragment>
+  )
+}
